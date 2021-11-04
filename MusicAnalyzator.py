79d764122e697db9f59f2dd07531ae6c884d4e72
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-05 01:35:50 +0200

    added function to find sum in different parts of map

--- MusicAnalyzator.py
+++ MusicAnalyzator.py
@@ -1,5 +1,17 @@
 import json
 
+
+def find_summary_length(map_to_compute, result_arr, index, thread_n):
+    start_index = round(len(map_to_compute) / thread_n * index)
+    end_index = round(len(map_to_compute) / thread_n * (index + 1))
+    el_sum = 0
+
+    for key in list(map_to_compute.keys())[start_index: end_index]:
+        el_sum = el_sum + map_to_compute[key]['meta']['track']['duration']
+    result_arr[index] = el_sum
+
+
 with open("classical.json", "r", encoding="utf-8") as file:
-    text = json.load(file)
-    print(text)
+    threads_num = 8
+
+    values = json.load(file)
